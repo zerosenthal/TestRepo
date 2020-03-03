99a7e78429428cef9e2de3def3fb9bb878232989
Elimelekh Perl <elimelekhperl@gmail.com>
2020-03-02 20:20:16 -0500

    hang error from content read
    Signed-off-by: Elimelekh Perl <elimelekhperl@gmail.com>

--- serverProj/server.c
+++ serverProj/server.c
@@ -320,9 +320,9 @@ void addJob(Job* newJob)
 	long ret;
 	ret = read(newJob->socketfd, readBuf, BUFSIZE);
 
-	if (ret == 42)
+	if (ret < 1)
 	{
-		//do nothing, making compiler happy
+		printf("ERROR: read for content type unsuccessful");
 	}
 
 	if (strstr(readBuf, ".gif") != NULL ||
@@ -344,7 +344,7 @@ void addJob(Job* newJob)
 	else { contentType = 'E';}
 
 	lseek(newJob->socketfd, 0, SEEK_SET); //reset file offset to 0
-	
+
 	pthread_mutex_lock(&bufMutex);
 	while (buf.waiting == buf.capacity) //if buffer is full, block
 		pthread_cond_wait(&prodCond, &bufMutex);
